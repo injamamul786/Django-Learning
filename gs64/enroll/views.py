9569ab80c7285ca5778eacd45174841503537029
INJAMAMUL HAQ <61545224+injamamul786@users.noreply.github.com>
2023-03-25 12:12:05 +0530

    Add files via upload

---
+++ gs64/enroll/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import Signup
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+# Create your views here.
+
+#Sign up
+def sign_up(request):
+    if request.method=='POST':
+        fm = Signup(request.POST)
+        if fm.is_valid():
+            messages.success(request, 'Your account created successfully')
+            fm.save()
+
+    else: 
+        fm = Signup()
+    return render(request, 'enroll/signup.html', {'form':fm})
+
+#login
+def userLogin(request):
+    if not request.user.is_authenticated:
+        if request.method=="POST":
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                pw = fm.cleaned_data['password']
+                valid_user = authenticate(username=uname, password=pw)
+                if valid_user:
+                    login(request, valid_user)
+                    messages.success(request, "Login successefully")
+                    return HttpResponseRedirect(f'/profile/{uname}/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html', {"form":fm})
+    else:
+        return HttpResponseRedirect(f'/profile/{request.user}/')
+
+#Profile
+def profile(request, username):
+    if request.user.is_authenticated:
+        return render(request, 'enroll/profile.html', {'name':request.user})
+    else:
+        return HttpResponseRedirect('/login/')
+    
+#Logout
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+#Change Password with old password
+def changePass(request):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            fm = PasswordChangeForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                update_session_auth_hash(request, fm.user)
+                return HttpResponseRedirect(f'/profile/{fm.user}/')
+        else:
+            fm  = PasswordChangeForm(user=request.user)
+        return render(request, 'enroll/changepass.html', {'form':fm})
+    else:
+        return HttpResponseRedirect('/login/')
+
+#without old password change password
+def changePass1(request):
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            fm = SetPasswordForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                update_session_auth_hash(request, fm.user)
+                return HttpResponseRedirect(f'/profile/{fm.user}/')
+        else:
+            fm  = SetPasswordForm(user=request.user)
+        return render(request, 'enroll/changepassword.html', {'form':fm})
+    else:
+        return HttpResponseRedirect('/login/')
---
+++ gs65/gs65/urls.py
@@ -0,0 +1,28 @@
+"""gs65 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from enroll import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.sign_up, name='signup'),
+    path('login/', views.userLogin, name='login'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('logout/', views.user_logout, name='logout'),
+    path('change password/', views.changePass, name='ChangePass'),
+    path('update password/', views.changePass1, name='ChangePass1'),
+]
